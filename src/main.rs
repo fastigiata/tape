cd832cdde0b03ae71b348363688e07da5ce620b6
lopo <lopo@zju.edu.cn>
2023-09-14 18:30:45 +0800

    test: toast test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use std::path::Path;
+use tauri_winrt_notification::{IconCrop, Toast};
+
 mod act;
 mod app;
 mod canonicalize;
@@ -5,4 +8,12 @@ mod record;
 
 fn main() {
     println!("Hello, world!");
+
+    // Toast::new(Toast::POWERSHELL_APP_ID)
+    //     .hero(Path::new(r"D:\GitProject\tape\idea.png"), "Tape")
+    //     .title("Hello, world!")
+    //     .text1("This is a notification")
+    //     .icon(Path::new(r"D:\GitProject\tape\idea.png"), IconCrop::Square, "Tape")
+    //     .show()
+    //     .unwrap();
 }
