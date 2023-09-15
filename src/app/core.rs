18cde246d361d5f0fd695af6e2b0df8d6d87a276
lopo <lopo@zju.edu.cn>
2023-09-16 00:13:31 +0800

    feat: render of app_banner

--- src/app/core.rs
+++ src/app/core.rs
@@ -1,13 +1,9 @@
 use eframe::{egui, glow};
-use eframe::egui::{Align, Align2, CentralPanel, Color32, CursorIcon, FontId, Id, RichText, Sense, Stroke, Visuals};
+use eframe::egui::{Align, Align2, CentralPanel, FontId, Id, Sense, Visuals};
 
 // region Constants
-const APP_COLOR: Color32 = Color32::from_rgb(252, 252, 252);
-const APP_BORDER_WIDTH: f32 = 0.5;
-const APP_BORDER_COLOR: Color32 = Color32::from_rgb(0, 0, 0);
 const APP_BORDER_RADIUS: f32 = 8.0;
 const APP_BANNER_H: f32 = 32.0;
-const APP_BANNER_BUTTON_H: f32 = 12.0;
 // endregion
 
 // region Helpers
@@ -15,7 +11,7 @@ const APP_BANNER_BUTTON_H: f32 = 12.0;
 pub enum AppState { Idle, Record, Act }
 
 #[derive(Debug, PartialEq)]
-pub enum AppRoute { Home, Record, Act, List }
+pub enum AppRoute { Home, Record, Act, List, About }
 
 impl AppRoute {
     pub fn name(&self) -> String {
@@ -24,12 +20,14 @@ impl AppRoute {
             AppRoute::Record => "Record",
             AppRoute::Act => "Act",
             AppRoute::List => "List",
+            AppRoute::About => "About",
         }.to_string()
     }
 }
 // endregion
 
 pub struct TapeApp {
+    dark_mode: bool,
     app_state: AppState,
     app_route: AppRoute,
 }
@@ -37,6 +35,7 @@ pub struct TapeApp {
 impl TapeApp {
     pub fn new() -> TapeApp {
         TapeApp {
+            dark_mode: false,
             app_state: AppState::Idle,
             app_route: AppRoute::Home,
         }
@@ -45,10 +44,10 @@ impl TapeApp {
     /// Render the entire app (including the banner and outlet)
     fn render_app(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
         let app_frame = egui::Frame {
-            fill: APP_COLOR,
+            fill: ctx.style().visuals.window_fill(),
             rounding: APP_BORDER_RADIUS.into(),
-            stroke: (APP_BORDER_WIDTH, APP_BORDER_COLOR).into(),
-            outer_margin: APP_BORDER_WIDTH.into(),
+            stroke: ctx.style().visuals.widgets.noninteractive.fg_stroke,
+            outer_margin: 0.5.into(),
             ..Default::default()
         };
 
@@ -79,8 +78,8 @@ impl TapeApp {
             rect.center(),
             Align2::CENTER_CENTER,
             self.app_route.name(),
-            FontId::proportional(16.0),
-            Color32::BLACK,
+            FontId::proportional(20.0),
+            ui.style().visuals.text_color(),
         );
 
         // paint the separator line
@@ -89,7 +88,7 @@ impl TapeApp {
                 rect.left_bottom() + egui::vec2(1.0, 0.0),
                 rect.right_bottom() + egui::vec2(-1.0, 0.0),
             ],
-            Stroke::from((APP_BORDER_WIDTH, APP_BORDER_COLOR)),
+            ui.visuals().widgets.noninteractive.bg_stroke,
         );
 
         // interact with the banner -- drag to move the window
@@ -98,37 +97,36 @@ impl TapeApp {
             frame.drag_window();
         }
 
-        // operate the banner -- minimize & close
+        // operate the banner -- dark/light mode, minimize, close
         ui.allocate_ui_at_rect(rect, |ui| {
             ui.with_layout(egui::Layout::right_to_left(Align::Center), |ui| {
-                ui.spacing_mut().item_spacing.x = 0.0;
-                ui.visuals_mut().button_frame = false;
                 ui.add_space(8.0);
+                ui.visuals_mut().button_frame = false;
 
                 // close the window
-                if ui.button(RichText::new("❌").size(APP_BANNER_BUTTON_H))
-                    .on_hover_cursor(CursorIcon::PointingHand)
-                    .on_hover_text_at_pointer("Close the window")
+                if ui.button("X")
+                    .on_hover_text("Close the window")
                     .clicked() {
                     frame.close();
                 }
 
                 // minimize the window
-                if ui.button(RichText::new("🗕").size(APP_BANNER_BUTTON_H))
-                    .on_hover_cursor(CursorIcon::PointingHand)
-                    .on_hover_text_at_pointer("Minimize the window")
+                if ui.button("·")
+                    .on_hover_text("Minimize the window")
                     .clicked() {
                     frame.set_minimized(true);
                 }
+
+                // switch between dark & light mode
+                egui::widgets::global_dark_light_mode_switch(ui);
             });
         });
-
-
-        // TODO: https://github.com/emilk/egui/blob/master/examples/custom_window_frame/src/main.rs L123
     }
 
     /// Render the outlet
-    fn render_outlet(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {}
+    fn render_outlet(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
+        // TODO: render the outlet following the 'app_route'
+    }
 }
 
 impl eframe::App for TapeApp {
