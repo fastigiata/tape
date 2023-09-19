f6cb50c40f179a7856b5e27a4bc74157bc07aebe
lopo <lopo@zju.edu.cn>
2023-09-19 23:38:33 +0800

    feat: declaration & re-export of renderer

--- src/app/pages/mod.rs
+++ src/app/pages/mod.rs
@@ -1,1 +1,9 @@
-mod root;
+mod home;
+
+use eframe::egui::{Rect, Ui};
+use crate::app::core::TapeApp;
+
+/// A renderer to render a page
+pub type PageRenderer = fn(app: &mut TapeApp, rect: Rect, ui: &mut Ui, frame: &mut eframe::Frame);
+
+pub use home::home_renderer;
