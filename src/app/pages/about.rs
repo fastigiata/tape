d79f62dbc4de5ca6adb305b3918306c15cac48d2
lopo <lopo@zju.edu.cn>
2023-09-21 00:12:19 +0800

    test: font test

--- src/app/pages/about.rs
+++ src/app/pages/about.rs
@@ -1,8 +1,11 @@
 use eframe::egui::{Rect, Ui};
 use crate::app::core::{AppRoute, TapeApp};
+use crate::app::prefabs;
 
 pub fn about_renderer(app: &mut TapeApp, rect: Rect, ui: &mut Ui, frame: &mut eframe::Frame) {
     ui.allocate_ui_at_rect(rect, |ui| {
+        ui.label(prefabs::text_en("Powered by egui", 16.0));
+        ui.label(prefabs::text_zh("Powered by egui", 16.0));
         ui.label("This is just the contents of the window.");
 
         if ui.button("Go to Home").clicked() {
