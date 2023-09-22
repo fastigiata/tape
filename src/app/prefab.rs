6a7b5329ab9523cde6732b66e6a8f0b0d09ecf71
lopo <lopo@zju.edu.cn>
2023-09-23 00:03:11 +0800

    refactor: file tree

--- src/app/prefab.rs
+++ src/app/prefab.rs
@@ -1,13 +1,4 @@
-use eframe::egui::{Button, FontFamily, FontId, RichText};
-
-pub fn button(text: &str) -> Button {
-    Button::new(
-        RichText::new(text)
-            .font(FontId::proportional(16.0))
-    )
-        .rounding(4.0)
-        .frame(false)
-}
+use eframe::egui::{FontFamily, FontId, RichText};
 
 pub fn text_en(text: &str, size: f32) -> RichText {
     RichText::new(text)
