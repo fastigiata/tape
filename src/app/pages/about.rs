dd3d33caa674dab6112d2a1e902f08856c88bda3
lopo <lopo@zju.edu.cn>
2023-09-23 00:05:57 +0800

    feat: page -- about

--- src/app/pages/about.rs
+++ src/app/pages/about.rs
@@ -1,41 +1,58 @@
 use eframe::egui;
-use eframe::egui::{Align, Rect, Ui};
-use crate::app::core::{AppRoute, TapeApp};
-use crate::app::prefabs;
+use eframe::egui::{Align, Rect, Separator, Ui};
+use eframe::egui::output::OpenUrl;
+use crate::app::core::{TapeApp};
+use crate::app::prefab;
 
-const TAPE_DESC_ZH: &'static str = "Tape 是一个用于记录鼠标和/或键盘操作的应用程序，可以生成记录文件用于稍后播放。";
+const TAPE_DESC_ZH: &'static str = "Tape 是一个用于记录鼠标和/或键盘操作的应用，可以生成记录文件用于稍后播放。";
 const TAPE_DESC_EN: &'static str = "Tape is an application for recording mouse and/or keyboard actions, which can generate a recording file for later playback.";
+const TAPE_LICENSE_URL: &'static str = "https://github.com/lopo12123/tape/blob/master/LICENSE";
 
 pub fn about_renderer(app: &mut TapeApp, rect: Rect, ui: &mut Ui, frame: &mut eframe::Frame) {
-    ui.allocate_ui_at_rect(rect, |ui| {
-        ui.with_layout(egui::Layout::top_down(Align::Center), |ui| {
-            // ===== section: description
-            ui.label(prefabs::text_zh("说明 / Description", 21.0));
-            ui.label(prefabs::text_zh(TAPE_DESC_ZH, 16.0));
-            ui.label(prefabs::text_en(TAPE_DESC_EN, 16.0));
-            // TODO: divider line
-        });
+    ui.allocate_ui_at_rect(
+        {
+            let mut v = rect;
+            v.min += egui::vec2(16.0, 16.0);
+            v.max -= egui::vec2(16.0, 16.0);
+            v
+        },
+        |ui| {
+            ui.with_layout(egui::Layout::top_down(Align::Center), |ui| {
+                // ===== section: description (zh)
+                ui.label(prefab::text_zh("说明", 24.0));
+                ui.label(prefab::text_zh(TAPE_DESC_ZH, 16.0));
 
-        ui.style().text_styles();
+                // separator
+                ui.add(Separator::default().spacing(16.0));
 
-        if ui.button("Go to Home").clicked() {
-            app.set_app_route(AppRoute::Home);
-        }
+                // ===== section: license (zh)
+                ui.label(prefab::text_zh("许可证", 24.0));
+                ui.label(prefab::text_en("MIT. Copyright (c) 2023 lopo12123", 16.0));
+                if ui.link(prefab::text_zh("点击此处查看详情", 16.0)).clicked() {
+                    ui.ctx().output_mut(|o| {
+                        o.open_url = Some(OpenUrl::same_tab(TAPE_LICENSE_URL));
+                    });
+                }
 
-        if ui.button("Go to Record").clicked() {
-            app.set_app_route(AppRoute::Record);
-        }
+                // separator
+                ui.add(Separator::default().spacing(16.0));
 
-        if ui.button("Go to Act").clicked() {
-            app.set_app_route(AppRoute::Act);
-        }
+                // ===== section: description (en)
+                ui.label(prefab::text_en("Description", 24.0));
+                ui.label(prefab::text_en(TAPE_DESC_EN, 16.0));
 
-        if ui.button("Go to List").clicked() {
-            app.set_app_route(AppRoute::History);
-        }
+                // separator
+                ui.add(Separator::default().spacing(16.0));
 
-        if ui.button("Go to About").clicked() {
-            app.set_app_route(AppRoute::About);
-        }
-    });
+                // ===== section: license (en)
+                ui.label(prefab::text_en("License", 24.0));
+                ui.label(prefab::text_en("MIT. Copyright (c) 2023 lopo12123", 16.0));
+                if ui.link(prefab::text_en("Click here for details", 16.0)).clicked() {
+                    ui.ctx().output_mut(|o| {
+                        o.open_url = Some(OpenUrl::same_tab(TAPE_LICENSE_URL));
+                    });
+                }
+            });
+        },
+    );
 }
