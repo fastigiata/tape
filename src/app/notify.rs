1b90e2400d68662ed2cd7757790d941cdedb6cee
lopo <lopo@zju.edu.cn>
2023-10-02 22:33:59 +0800

    feat: implement 'Notifier'

--- src/app/notify.rs
+++ src/app/notify.rs
@@ -1,1 +1,29 @@
 //! This file contains the notifier of the app.
+
+use tauri_winrt_notification::{Duration, Toast};
+
+const APP_ID: &'static str = "tray_clock";
+// const APP_ID: &'static str = "tape";
+
+pub struct Notifier {}
+
+impl Notifier {
+    pub fn tell(detail: &str) {
+        Toast::new(APP_ID)
+            .title("Tape")
+            .text1(detail)
+            .duration(Duration::Short)
+            .show()
+            .expect("Failed to show toast");
+    }
+}
+
+#[cfg(test)]
+mod unit_test {
+    use super::*;
+
+    #[test]
+    fn ttt() {
+        Notifier::tell("hello");
+    }
+}
