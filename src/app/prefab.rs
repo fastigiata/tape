70f7d30fa7a93c70c4949d6b3be3f903f148c28f
lopo <lopo@zju.edu.cn>
2023-10-03 21:26:14 +0800

    test: iconfont

--- src/app/prefab.rs
+++ src/app/prefab.rs
@@ -1,19 +1,25 @@
 //! This file contains the prefabs of the app. Includes the following:
 //! - Text
 use eframe::egui::{FontFamily, FontId, RichText};
+use crate::app::prelude::FontIcon;
+
+#[deprecated(note = "not implemented")]
+pub fn font_icon(icon: FontIcon, size: f32) -> RichText {
+    RichText::new(icon.code()).font(FontId { size, family: FontFamily::Name("iconfont".into()) })
+}
 
 pub fn text_en(text: impl Into<String>, size: f32) -> RichText {
-    RichText::new(text)
-        .font(FontId {
-            size,
-            family: FontFamily::Name("Mooli".into()),
-        })
+    RichText::new(text).font(FontId { size, family: FontFamily::Name("Mooli".into()) })
 }
 
 pub fn text_zh(text: impl Into<String>, size: f32) -> RichText {
-    RichText::new(text)
-        .font(FontId {
-            size,
-            family: FontFamily::Name("MaShanZheng".into()),
-        })
+    RichText::new(text).font(FontId { size, family: FontFamily::Name("MaShanZheng".into()) })
+}
+
+#[cfg(test)]
+mod unit_test {
+    #[test]
+    fn tt() {
+        println!("\u{e613}");
+    }
 }
