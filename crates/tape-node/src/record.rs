5db75188b53c8ead55963569ae073f62c60c4e4d
lopo <lopo@zju.edu.cn>
2023-10-15 16:07:07 +0800

    feat: basic recorder

---
+++ crates/tape-node/src/record.rs
@@ -0,0 +1,33 @@
+use tape_core::record::Recorder;
+
+#[napi(js_name = "Recorder")]
+pub struct NodeRecorder {
+    inner: Recorder,
+}
+
+#[napi]
+impl NodeRecorder {
+    #[napi(constructor)]
+    pub fn new(record_type: String, stop_signal: Option<String>) -> NodeRecorder {
+        NodeRecorder {
+            inner: Recorder::new(record_type.into(), stop_signal.map(Into::into))
+        }
+    }
+
+    #[napi]
+    pub fn set_record_type(&mut self, record_type: String) {
+        self.inner.set_record_type(record_type.into());
+    }
+
+    #[napi]
+    pub fn set_stop_signal(&mut self, stop_signal: Option<String>) {
+        self.inner.set_stop_signal(stop_signal.map(Into::into));
+    }
+
+    #[napi]
+    pub fn record(&mut self) {
+        self.inner.record(Some(Box::new(|script| {
+            println!("done!");
+        })));
+    }
+}
