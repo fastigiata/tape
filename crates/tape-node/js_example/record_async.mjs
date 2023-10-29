caf366ee78de74533a9853cfecb6c6651c23ee34
lopo <lopo@zju.edu.cn>
2023-10-29 22:08:21 +0800

    test: test & declare

--- crates/tape-node/js_example/record_async.mjs
+++ crates/tape-node/js_example/record_async.mjs
@@ -1,9 +1,13 @@
-import {Recorder} from "../index";
-// import {Recorder} from "../index.js";
+import {Recorder} from "../index.js";
 
 const recorder = new Recorder('keyboard')
 
 // this call will rejected directly cause no stop-signal is specified
+
+// note: Due to the overhead required for inter-thread communication,
+// setting stop-signal immediately after calling recordAsync on a recorder
+// that does not set it will also work (but this is undefined behavior and
+// is not guaranteed to always work)
 recorder.recordAsync()
     .then((v) => {
         console.log('[recordAsync 1] done:', v)
@@ -12,19 +16,19 @@ recorder.recordAsync()
         console.log('[recordAsync 1] error:', err)
     })
 
-// then we specify the stop-signal and call again
-recorder.setStopSignal('esc')
-// now it works, and the async call won't be finished until the specified key is pressed
-recorder.recordAsync()
-    .then((v) => {
-        console.log('[recordAsync 2] done:', v)
-    })
-    .catch(err => {
-        console.log('[recordAsync 2] error:', err)
-    })
-
-// here, you can do anything you want in the main thread and the recorder won't block it
-console.log('this will not be blocked and will be printed immediately')
-
-// The 'recording thread' will automatically join the main thread
-// to prevent the child thread from being automatically killed when the main thread ends.
+// // then we specify the stop-signal and call again
+// recorder.setStopSignal('esc')
+// // now it works, and the async call won't be finished until the specified key is pressed
+// recorder.recordAsync()
+//     .then((v) => {
+//         console.log('[recordAsync 2] done:', v)
+//     })
+//     .catch(err => {
+//         console.log('[recordAsync 2] error:', err)
+//     })
+//
+// // here, you can do anything you want in the main thread and the recorder won't block it
+// console.log('this will not be blocked and will be printed immediately')
+//
+// // The 'recording thread' will automatically join the main thread
+// // to prevent the child thread from being automatically killed when the main thread ends.
