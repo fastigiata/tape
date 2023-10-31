22aac598e8b1cb84aa123538cd190911cf7ae84b
lopo <lopo@zju.edu.cn>
2023-10-31 14:55:40 +0800

    feat: act_callback

--- crates/tape-node/src/act.rs
+++ crates/tape-node/src/act.rs
@@ -1,5 +1,9 @@
 use std::sync::{Arc, Mutex};
-use napi::{Env, Error, Status, Task};
+use napi::{
+    bindgen_prelude::{AsyncTask, Result},
+    Env, Error, JsFunction, JsUndefined, Status, Task,
+    threadsafe_function::{ErrorStrategy, ThreadsafeFunction, ThreadsafeFunctionCallMode},
+};
 use tape_core::act::Actor;
 use crate::ffi_adapter::FFISafeScript;
 
@@ -13,7 +17,7 @@ impl Task for AsyncActor {
 
     fn compute(&mut self) -> napi::Result<Self::Output> {
         self.worker.lock().unwrap().act_sync()
-            .map_err(|_| Error::new(Status::WouldDeadlock, "no stop signal set before calling record_async"))
+            .map_err(|_| Error::new(Status::WouldDeadlock, "no stop signal set before calling act_async"))
     }
 
     fn resolve(&mut self, _env: Env, _output: Self::Output) -> napi::Result<Self::JsValue> {
@@ -35,8 +39,8 @@ impl NodeActor {
     /// create a new actor
     /// - script: the script to act, can be changed later
     /// - cyclic: whether the actor is acting cyclically
-    /// - record_type: the type of the recorder, can be "keyboard", "mouse" or "both"
-    /// - stop_signal: the signal to stop the recorder, can be one of valid 'CanonicalKey'
+    /// - act_type: the type of the acter, can be "keyboard", "mouse" or "both"
+    /// - stop_signal: the signal to stop the acter, can be one of valid 'CanonicalKey'
     #[napi(constructor)]
     pub fn new(
         script: FFISafeScript,
@@ -48,6 +52,78 @@ impl NodeActor {
         let worker = Actor::new(script.into(), cyclic, act_type.into(), stop_signal.map(Into::into));
 
         NodeActor { inner: Arc::new(Mutex::new(worker)) }
+    }
+
+
+    /// Set the script to be acted.
+    ///
+    /// This has no effect on the current acting. (The script is cloned once `act_callback` or `act_async` is called)
+    #[napi]
+    pub fn new_script(&self, script: FFISafeScript) {
+        self.inner.lock().unwrap().new_script(script.into());
+    }
+
+    /// Set whether the actor is acting cyclically.
+    ///
+    /// Can affect the current acting cause 'cyclic' will be checked before every loop.
+    #[napi]
+    pub fn set_cyclic(&self, cyclic: bool) {
+        self.inner.lock().unwrap().set_cyclic(cyclic);
+    }
+
+    /// Set the type of the action to be acted.
+    ///
+    /// This has no effect on the current acting. (The script is cloned and filtered once `act_callback` or `act_async` is called)
+    #[napi]
+    pub fn set_act_type(
+        &self,
+        #[napi(ts_arg_type = "'keyboard' | 'mouse' | 'both'")]
+        act_type: String,
+    ) {
+        self.inner.lock().unwrap().set_act_type(act_type.into());
+    }
+
+    /// Set the key that stops the acting
+    ///
+    /// This has no effect on the current acting. (The signal is copied once `act_callback` or `act_async` is called.)
+    #[napi]
+    pub fn set_stop_signal(&self, stop_signal: Option<String>) {
+        self.inner.lock().unwrap().set_stop_signal(stop_signal.map(Into::into));
+    }
+
+    /// Start acting (The act will stop when the stop signal is received,
+    /// you can also use the `finish` to interrupt the acting manually).
+    ///
+    /// This will run in a separate thread (created by `std::thread::spawn`), so it will not block the main thread.
+    /// On the other hand, you may need to wait in the main thread for the acting to finish.
+    ///
+    /// ---
+    /// see `act_async` for promise-like usage
+    #[napi]
+    pub fn act_callback(
+        &self,
+        #[napi(ts_arg_type = "() => void")]
+        on_finish: JsFunction,
+    ) -> Result<()> {
+        let tsfn: ThreadsafeFunction<(), ErrorStrategy::Fatal> = on_finish
+            .create_threadsafe_function(0, |_ctx| {
+                let v: Vec<JsUndefined> = vec![];
+                Ok(v)
+            })?;
+
+        self.inner.lock().unwrap().act(Some(Box::new(move || {
+            tsfn.call((), ThreadsafeFunctionCallMode::NonBlocking);
+        })));
+
+        Ok(())
+    }
+
+    /// Interrupt the acting started by `act_callback`
+    #[napi]
+    pub fn finish(&self) -> Result<()> {
+        self.inner.lock().unwrap().finish();
+
+        Ok(())
     }
 
     // TODO
