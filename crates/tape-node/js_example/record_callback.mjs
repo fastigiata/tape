d70190ddea64d04116ec7af50231a742d50fe491
lopo <lopo@zju.edu.cn>
2023-10-31 14:54:46 +0800

    feat: log

--- crates/tape-node/js_example/record_callback.mjs
+++ crates/tape-node/js_example/record_callback.mjs
@@ -1,9 +1,13 @@
+import {writeFileSync} from "node:fs"
 import {Recorder} from "../index.js"
 
 const recorder = new Recorder('keyboard', 'esc')
 
 recorder.recordCallback((v) => {
     console.log('[recordCallback] done:', v)
+
+    // output the script to json file
+    writeFileSync('./sim.json', JSON.stringify(v), 'utf-8')
 })
 
 // here, you can do anything you want in the main thread and the recorder won't block it
@@ -14,6 +18,6 @@ console.log('this will not be blocked and will be printed immediately')
 
 // or you can call `recorder.finish()` to manually stop the recorder whenever you want,
 // here is an example, the recorder will be stopped after 5 seconds if you don't press the stop-signal
-setTimeout(() => {
-    recorder.finish()
-}, 5_000)
+// setTimeout(() => {
+//     recorder.finish()
+// }, 5_000)
